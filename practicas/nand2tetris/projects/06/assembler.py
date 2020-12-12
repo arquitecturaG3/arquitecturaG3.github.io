c7a647b2b14d71e1ae7e9d769c0a43a4e93870ec
DanielLizcano <66855818+DanielLizcano@users.noreply.github.com>
2020-12-11 20:26:37 -0500

    Update assembler.py

--- practicas/nand2tetris/projects/06/assembler.py
+++ practicas/nand2tetris/projects/06/assembler.py
@@ -32,6 +32,29 @@ def comp(cadena):
     'D|M': ['1', '0', '1', '0', '1', '0', '1']}
     return dic[cadena]
 
+def compDest(inDest): //metodo dest
+    Dest = {'None': (0,0,0), 
+           'M':(0,0,1), 
+           'D':(0,1,0), 
+           'MD':(0,1,1), 
+           'A':(1,0,0), 
+           'AM':(1,0,1), 
+           'AD':(1,1,0), 
+           'AMD':(1,1,1) }
+    return Dest[inDest]
+print(compDest("MD"))
+
+def jump_comp(jump):
+    jumps= {'None': (0,0,0), 
+    'JGT':(0,0,1),
+    'JEQ':(0,1,0),
+    'JGE':(0,1,1),
+    'JLT':(1,0,0), 
+    'JNE':(1,0,1),
+    'JLE':(1,1,0),
+    ''JMP:(1,1,1)}
+    return jumps[jump]
+
 def clear_file(lines):
     lines = [  ( '' if i[0:2] == '//' else i) for i in lines ]
     lines = [ i.replace('\n','') for i in lines]
